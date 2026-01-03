2f72d2bcb924a9c0c45308268aa06c34d34d3104
tonkaintl <150328651+tonkaintl@users.noreply.github.com>
2026-01-03 09:42:57 -0600

    fix name issue

--- src/controllers/tonkaDispatchNewsletters/methods.js
+++ src/controllers/tonkaDispatchNewsletters/methods.js
@@ -2,7 +2,7 @@ import { deleteNewsletter } from './methods/newsletters.controller.delete.js';
 import { removeArticle } from './methods/newsletters.controller.delete.remove-article.js';
 import { listNewsletters } from './methods/newsletters.controller.get.list.js';
 import { getNewsletter } from './methods/newsletters.controller.get.single.js';
-import { updateArticle } from './methods/newsletters.controller.patch.update-article.js';
+import { updateArticle } from './methods/newsletters.controller.patch.article-update.js';
 import { updateNewsletter } from './methods/newsletters.controller.patch.update.js';
 import { addArticle } from './methods/newsletters.controller.post.add-article.js';
 import { createNewsletter } from './methods/newsletters.controller.post.create.js';
